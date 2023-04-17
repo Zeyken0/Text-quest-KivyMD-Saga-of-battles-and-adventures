b27c8c2a280173abf428a8e51c396a13f9eced30
Zeyken_0 <56526742+Zeyken0@users.noreply.github.com>
2023-04-17 19:26:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ from kivymd.uix.fitimage import FitImage
 from kivymd.uix.button import MDFloatingActionButton
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
+from kivy.properties import StringProperty, ObjectProperty
+
+main_data = {'0_0': """Добро пожаловать в Сагу битв и Приключений
+                      вы готовы начать?""",
+             '1_0': """Поехали. Вы бомж.""",
+             '1_1': """Досвидания!!!"""}
 
 
 class MenuScreen(Screen):
@@ -12,8 +18,19 @@ class MenuScreen(Screen):
 
 
 class GameScreen(Screen):
-    pass
+    label_wig = ObjectProperty(defaultvalue=main_data['0_0'])
+    save_number = StringProperty(defaultvalue='0_0')
+    variant = StringProperty(defaultvalue='0')
 
+    def next_step(self, save_number, variant):
+        #print(main_data[str(eval(save_number[4:]+'+1'))])
+        if variant == '0':
+            save_number = str(eval(save_number.strip('_')[0]+'+1'))+'_'+variant
+            print('Current_step: '+save_number)
+        else:
+            save_number = str(eval(save_number.strip('_')[0] + '+1')) + '_' + variant
+            print('Current_step: '+save_number)
+        self.label_wig = main_data[save_number] + f"{save_number}"
 
 sm = ScreenManager()
 sm.add_widget(MenuScreen(name='menu'))
@@ -28,7 +45,9 @@ class TextQuest(MDApp):
     def build(self):
         screen = Builder.load_file('View/main.kv')
         return screen
-#white-background
+
+
+# white-background
 
 if __name__ == '__main__':
     TextQuest().run()
